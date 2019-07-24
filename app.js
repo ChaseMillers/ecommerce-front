05f7117c4089bd4fa0cc60e62ce7dfd30ff66de1
Chase Miller <chasemillers@outlook.com>
2019-07-23 20:09:30 -0700

    SignUp, SignIn, SignOut all working with postman

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require ('express');
+const mongoose = require ('mongoose');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+// validator must be below version 6 
+const expressValidator = require('express-validator')
+
+require ('dotenv').config();
+// import routes
+const userRoutes = require('./routes/user')
+
+// app
+const app = express();
+
+// db
+mongoose
+    .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true, 
+        useCreateIndex: true
+    })  
+    .then(() => console.log("DB Connected")); 
+
+// middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(expressValidator());
+
+// routes middleware
+app.use("/api", userRoutes);
+
+const port = process.env.PORT || 8000
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+});
+
+// .connect('mongodb+srv://Chase:poop@e-commerceapi-ojmxq.mongodb.net/test?retryWrites=true&w=majority',{
+//     useMongoClient: true  
+//   })  
