c33e8eedfc75b9085a8d3daed735e25cf9547767
Chase Miller <chasemillers@outlook.com>
2019-07-26 09:20:39 -0700

    added better functionality to search bar

--- src/core/Checkbox.js
+++ src/core/Checkbox.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { getProducts, getBraintreeClientToken, processPayment, createOrder } from './apiCore';
-// import { emptyCart } from './cartHelpers';
+import { emptyCart } from './cartHelpers';
 import Card from './Card';
 import { isAuthenticated } from '../auth';
 import { Link } from 'react-router-dom';
 // import "braintree-web"; // not using this package
-// import DropIn from 'braintree-web-drop-in-react';
+import DropIn from 'braintree-web-drop-in-react';
 
 const Checkout = ({ products }) => {
     const [data, setData] = useState({
@@ -58,66 +58,66 @@ const Checkout = ({ products }) => {
 
     let deliveryAddress = data.address;
 
-    // const buy = () => {
-    //     setData({ loading: true });
-    //     // send the nonce to your server
-    //     // nonce = data.instance.requestPaymentMethod()
-    //     let nonce;
-    //     let getNonce = data.instance
-    //         .requestPaymentMethod()
-    //         .then(data => {
-    //             // console.log(data);
-    //             nonce = data.nonce;
-    //             // once you have nonce (card type, card number) send nonce as 'paymentMethodNonce'
-    //             // and also total to be charged
-    //             // console.log(
-    //             //     "send nonce and total to process: ",
-    //             //     nonce,
-    //             //     getTotal(products)
-    //             // );
-    //             const paymentData = {
-    //                 paymentMethodNonce: nonce,
-    //                 amount: getTotal(products)
-    //             };
+    const buy = () => {
+        setData({ loading: true });
+        // send the nonce to your server
+        // nonce = data.instance.requestPaymentMethod()
+        let nonce;
+        let getNonce = data.instance
+            .requestPaymentMethod()
+            .then(data => {
+                // console.log(data);
+                nonce = data.nonce;
+                // once you have nonce (card type, card number) send nonce as 'paymentMethodNonce'
+                // and also total to be charged
+                // console.log(
+                //     "send nonce and total to process: ",
+                //     nonce,
+                //     getTotal(products)
+                // );
+                const paymentData = {
+                    paymentMethodNonce: nonce,
+                    amount: getTotal(products)
+                };
 
-    //             processPayment(userId, token, paymentData)
-    //                 .then(response => {
-    //                     console.log(response);
-    //                     // empty cart
-    //                     // create order
+                processPayment(userId, token, paymentData)
+                    .then(response => {
+                        console.log(response);
+                        // empty cart
+                        // create order
 
-    //                     const createOrderData = {
-    //                         products: products,
-    //                         transaction_id: response.transaction.id,
-    //                         amount: response.transaction.amount,
-    //                         address: deliveryAddress
-    //                     };
+                        const createOrderData = {
+                            products: products,
+                            transaction_id: response.transaction.id,
+                            amount: response.transaction.amount,
+                            address: deliveryAddress
+                        };
 
-    //                     createOrder(userId, token, createOrderData)
-    //                         .then(response => {
-    //                             emptyCart(() => {
-    //                                 console.log('payment success and empty cart');
-    //                                 setData({
-    //                                     loading: false,
-    //                                     success: true
-    //                                 });
-    //                             });
-    //                         })
-    //                         .catch(error => {
-    //                             console.log(error);
-    //                             setData({ loading: false });
-    //                         });
-    //                 })
-    //                 .catch(error => {
-    //                     console.log(error);
-    //                     setData({ loading: false });
-    //                 });
-    //         })
-    //         .catch(error => {
-    //             // console.log("dropin error: ", error);
-    //             setData({ ...data, error: error.message });
-    //         });
-    // };
+                        createOrder(userId, token, createOrderData)
+                            .then(response => {
+                                emptyCart(() => {
+                                    console.log('payment success and empty cart');
+                                    setData({
+                                        loading: false,
+                                        success: true
+                                    });
+                                });
+                            })
+                            .catch(error => {
+                                console.log(error);
+                                setData({ loading: false });
+                            });
+                    })
+                    .catch(error => {
+                        console.log(error);
+                        setData({ loading: false });
+                    });
+            })
+            .catch(error => {
+                // console.log("dropin error: ", error);
+                setData({ ...data, error: error.message });
+            });
+    };
 
     const showDropIn = () => (
         <div onBlur={() => setData({ ...data, error: '' })}>
@@ -133,7 +133,7 @@ const Checkout = ({ products }) => {
                         />
                     </div>
 
-                    {/* <DropIn
+                    <DropIn
                         options={{
                             authorization: data.clientToken,
                             paypal: {
@@ -141,10 +141,10 @@ const Checkout = ({ products }) => {
                             }
                         }}
                         onInstance={instance => (data.instance = instance)}
-                    /> */}
-                    {/* <button onClick={buy} className="btn btn-success btn-block">
+                    />
+                    <button onClick={buy} className="btn btn-success btn-block">
                         Pay
-                    </button> */}
+                    </button>
                 </div>
             ) : null}
         </div>
--- src/core/apiCore.js
+++ src/core/apiCore.js
@@ -1,5 +1,5 @@
 import { API } from "../config";
-// import queryString from "query-string";
+import queryString from "query-string";
 
 export const getProducts = sortBy => {
     return fetch(`${API}/products?sortBy=${sortBy}&order=desc&limit=6`, {
@@ -43,17 +43,17 @@ export const getFilteredProducts = (skip, limit, filters = {}) => {
         });
 };
 
-// export const list = params => {
-//     const query = queryString.stringify(params);
-//     console.log("query", query);
-//     return fetch(`${API}/products/search?${query}`, {
-//         method: "GET"
-//     })
-//         .then(response => {
-//             return response.json();
-//         })
-//         .catch(err => console.log(err));
-// };
+export const list = params => {
+    const query = queryString.stringify(params);
+    console.log("query", query);
+    return fetch(`${API}/products/search?${query}`, {
+        method: "GET"
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
 
 export const read = productId => {
     return fetch(`${API}/product/${productId}`, {
